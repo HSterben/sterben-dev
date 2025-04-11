28192d5b2b6c0fbc36534ecac5473c54f36c5469
Sterben <142906656+HSterben@users.noreply.github.com>
2025-04-10 23:48:08 -0400

    Last test for base link config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://hsterben.github.io",
-  base: '/sterben-dev/',
+  base: 'sterben-dev',
   trailingSlash: 'ignore',
   build: {
     assets: 'assets',
