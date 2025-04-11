0fd3aa13660c4b094ab7febb1d9cec11bbe88184
Sterben <142906656+HSterben@users.noreply.github.com>
2025-04-10 23:43:40 -0400

    added favicon logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://hsterben.github.io",
-  base: '/sterben-dev/',
-  trailingSlash: 'ignore',
-  build: {
-    assets: 'assets',
-  }
-});
+// export default defineConfig({
+//   site: "https://hsterben.github.io",
+//   base: '/sterben-dev/',
+//   trailingSlash: 'ignore',
+//   build: {
+//     assets: 'assets',
+//   }
+// });
+
+export default defineConfig({});
